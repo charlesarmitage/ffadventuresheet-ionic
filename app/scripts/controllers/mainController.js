e6e7a50a0adc342ad6ba8c64f7b0d106561e2d8d
charlesarmitage <code@cjrarmitage.co.uk>
2015-04-26 22:38:09 +0100

    Initial Ionic app

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name FantasyAdventureSheet.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('FantasyAdventureSheet')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name FantasyAdventureSheet.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('FantasyAdventureSheet')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
